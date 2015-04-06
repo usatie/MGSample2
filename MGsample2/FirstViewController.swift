a6cd2f5b12be775d2491c1b457f7d481adfc6d36
usatie <usatie@gmail.com>
2015-04-06 20:37:08 +0900

    Initial Commit

---
+++ MGsample2/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MGsample2
+//
+//  Created by Shun Usami on 2015/04/06.
+//  Copyright (c) 2015年 ShunUsami. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
