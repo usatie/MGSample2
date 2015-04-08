856ad16d834ca658ae04e3a86554d06f50ac58b8
usatie <usatie@gmail.com>
2015-04-09 04:28:31 +0900

    [add] ChangeViewController

---
+++ ChangeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ChangeViewController.swift
+//  MGsample2
+//
+//  Created by Shun Usami on 2015/04/09.
+//  Copyright (c) 2015年 ShunUsami. All rights reserved.
+//
+
+import UIKit
+
+class ChangeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
