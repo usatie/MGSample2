1d147c8768b37b8ad7c191f77c78310d6a05687b
usatie <usatie@gmail.com>
2015-04-09 12:02:30 +0900

    [make] ChangeViewController

--- ChangeViewController.swift
+++ ChangeViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ChangeViewController: UIViewController {
 
-    var employeeHireDic = [String:[String:Int]]()
+    var employeeChangeDic = [String:[String:Int]]()
     var market = ""
     var type = ""
     
@@ -21,26 +21,21 @@ class ChangeViewController: UIViewController {
         println("hiring")
         self.loadTemplate()
         var initialMarketDic = ["Search":0,"Local":0, "Entertainment":0, "News":0, "Commerce":0, "SNS":0]
-        employeeHireDic = ["Marketer":initialMarketDic,"Engineer":initialMarketDic,"Sales":initialMarketDic]
-        let typeArray = ["Marketer_before","Marketer_after","Engineer_before","Engineer_after"]
-        for i in 0...3 {
+        employeeChangeDic = ["Marketer":employeesDic["Marketer"]!,"Engineer":employeesDic["Engineer"]!]
+        for i in 0...1 {
             for j in 0...5 {
                 var marketView:MarketView = MarketView(frame: CGRectMake(220 + 180*CGFloat(i), 100+100*CGFloat(j), 180, 100));
                 marketView.plusButton.addTarget(self, action:Selector("plusButtonTapped:") , forControlEvents: UIControlEvents.TouchDown)
                 marketView.minusButton.addTarget(self, action:Selector("minusButtonTapped:") , forControlEvents: UIControlEvents.TouchDown)
                 marketView.tag = j+1
-                marketView.type = typeArray[i]//jobTypeArray[i]
+                marketView.type = jobTypeArray[i]//jobTypeArray[i]
                 self.view.addSubview(marketView)
                 marketView.propertyLabel.hidden = false
-                if i%2 == 0 {
-                    var employee = employeesDic[jobTypeArray[i/2]]![marketNameArray[j]]!
-                    marketView.propertyLabel.text = "(\(employee))"
-                }
+                var employee = employeesDic[jobTypeArray[i]]![marketNameArray[j]]!
+                marketView.propertyLabel.text = "(\(employee))"
+                marketView.numberLabel.text = "\(employee)"
                 //name of the market label
                 if (i == 0){
-                    var marketer = employeesDic[jobTypeArray[0]]![marketNameArray[j]]!
-                    var engineer = employeesDic[jobTypeArray[1]]![marketNameArray[j]]!
-                    var sales = employeesDic[jobTypeArray[2]]![marketNameArray[j]]!
                     let nameLabel = UILabel(frame: CGRectMake(20, 100+100*CGFloat(j), 200, 100))
                     nameLabel.text = "\(marketNameArray[j])"
                     self.view.addSubview(nameLabel)
@@ -61,13 +56,13 @@ class ChangeViewController: UIViewController {
     
     //Check the action's availability
     func canDeduct() -> Bool {
-        if(employeeHireDic[type]![market] > 0){ return true}
+        if(employeeChangeDic[type]![market] > 0){ return true}
         else {return false}
     }
     
     func canAdd() -> Bool {
-        let sum = Array(employeeHireDic["Marketer"]!.values).reduce(0, +) + Array(employeeHireDic["Engineer"]!.values).reduce(0, +) + Array(employeeHireDic["Sales"]!.values).reduce(0, +)
-        if(cashBalance > sum*10) {return true}
+        let sum = getDiff("Marketer") + getDiff("Engineer")
+        if(cashBalance > sum*5 && Array(employeesDic[type]!.values).reduce(0, +) > Array(employeeChangeDic[type]!.values).reduce(0, +)) {return true}
         else {return false}
     }
     
@@ -80,8 +75,8 @@ class ChangeViewController: UIViewController {
         type = marketView.type
         if(self.canDeduct()){
             println("\(market) hired less")
-            employeeHireDic[type]![market] = employeeHireDic[type]![market]! - 1
-            marketView.numberLabel.text = "\(employeeHireDic[type]![market]!)"
+            employeeChangeDic[type]![market] = employeeChangeDic[type]![market]! - 1
+            marketView.numberLabel.text = "\(employeeChangeDic[type]![market]!)"
         }
     }
     func plusButtonTapped(sender: AnyObject) {
@@ -92,21 +87,63 @@ class ChangeViewController: UIViewController {
         type = marketView.type
         if (self.canAdd()) {
             println("\(market) hired more")
-            employeeHireDic[type]![market] = employeeHireDic[type]![market]! + 1
-            marketView.numberLabel.text = "\(employeeHireDic[type]![market]!)"
+            employeeChangeDic[type]![market] = employeeChangeDic[type]![market]! + 1
+            marketView.numberLabel.text = "\(employeeChangeDic[type]![market]!)"
         }
     }
+    func getDiff(jobType: String) -> Int {
+        var sum = 0
+        for object:String in marketNameArray {
+            sum += abs(employeesDic[jobType]![object]! - employeeChangeDic[jobType]![object]!)
+        }
+        return sum/2
+    }
+    
+    func shouldChange(jobType: String) -> Bool {
+        return Array(employeesDic[jobType]!.values).reduce(0, +) == Array(employeeChangeDic[jobType]!.values).reduce(0, +)
+    }
     @IBAction func changeButtonPushed(sender: AnyObject) {
-        for object : String in marketNameArray{
-            for jobType in jobTypeArray {
-                cashBalance -= employeeHireDic[jobType]![object]!*10
-                employeesDic[jobType]![object] = employeesDic[jobType]![object]! + employeeHireDic[jobType]![object]!
-                var employee = employeesDic[jobType]![object]!
-                println("\(object) = \(employee)")
+        let marketerDiff = abs(Array(employeesDic["Marketer"]!.values).reduce(0, +) - Array(employeeChangeDic["Marketer"]!.values).reduce(0, +))
+
+        let engineerDiff = abs(Array(employeesDic["Engineer"]!.values).reduce(0, +) - Array(employeeChangeDic["Engineer"]!.values).reduce(0, +))
+
+        if(shouldChange("Marketer") && shouldChange("Engineer")) {
+            println("\(cashBalance) before")
+            cashBalance -= getDiff("Marketer")*5 + getDiff("Engineer")*5
+            println("\(cashBalance) after")
+            for object : String in marketNameArray{
+                for jobType in jobTypeArray {
+                    if jobType != "Sales"{
+                        employeesDic[jobType]![object] = employeeChangeDic[jobType]![object]!
+                        var employee = employeesDic[jobType]![object]!
+                        println("\(object) \(jobType) = \(employee)")
+                    }
+                }
             }
+            
+            self.dismissViewControllerAnimated(true, completion: nil)
+        } else {
+            println("Cannot dismiss any employees")
+            
+            //UIAlertView
+            let alert:UIAlertController = UIAlertController(title:"解雇できません",
+                message: "社員は解雇できません。全員どこかのサービスに振り分けてあげてください。\n企画：あと\(marketerDiff)人\n開発：あと\(engineerDiff)人",
+                preferredStyle: UIAlertControllerStyle.Alert)
+            
+            //Cancel 一つだけしか指定できない
+            let cancelAction:UIAlertAction = UIAlertAction(title: "OK",
+                style: UIAlertActionStyle.Cancel,
+                handler:{
+                    (action:UIAlertAction!) -> Void in
+                    println("Cancel")
+            })
+            
+            //AlertもActionSheetも同じ
+            alert.addAction(cancelAction)
+            
+            presentViewController(alert, animated: true, completion: nil)
         }
         
-        self.dismissViewControllerAnimated(true, completion: nil)
     }
 
 }
